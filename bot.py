dfba038109e02fa20215c131dca762a47d9b7bcb
Kacper Staron <staronkacper99@gmail.com>
2020-12-09 13:21:50 +0100

    first draftof getting channel history

--- bot.py
+++ bot.py
@@ -1,15 +1,25 @@
 import os
 import discord
 from dotenv import load_dotenv
+from discord.ext import commands
+import random
 
 load_dotenv()
 
 TOKEN = os.getenv('DISCORD_TOKEN')
 
-client = discord.Client()
+bot = commands.Bot(command_prefix='!')
 
-@client.event
+@bot.event
 async def on_ready():
-    print(f'{client.user} has connected to Discord!')
+    print(f'{bot.user.name} has connected to Discord!')
 
-client.run(TOKEN)
+
+@bot.command(name='hist')
+async def hist(ctx, arg: int):
+    messages = await ctx.channel.history(limit=200).flatten()
+    print(messages[arg])
+    print(len(messages))
+
+
+bot.run(TOKEN)
